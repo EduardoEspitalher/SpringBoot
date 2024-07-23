215223f6250aaf04a1527e9463f3ee2fca7ad26d
Eduardo Espitalher Dias <eduardo.espitalher.93@gmail.com>
2024-07-23 19:24:35 -0300

    saving and retrieving names in the database

--- src/main/java/curse/spring/boot/SalesApplication.java
+++ src/main/java/curse/spring/boot/SalesApplication.java
@@ -1,11 +1,31 @@
 package curse.spring.boot;
 
 
+
+import curse.spring.boot.domain.entity.Cliente;
+import curse.spring.boot.domain.repositorio.Clientes;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.List;
 
 @SpringBootApplication
 public class SalesApplication {
+
+    @Bean
+    public CommandLineRunner init(@Autowired Clientes clientes){
+        return args -> {
+            System.out.println("Salvando clientes");
+            clientes.salvar(new Cliente("Eduardo"));
+            clientes.salvar(new Cliente("Kallel"));
+
+            List<Cliente> todosClientes = clientes.obterTodos();
+            todosClientes.forEach(System.out::println);
+       };
+    }
 
     public static void main(String[] args) {
 
