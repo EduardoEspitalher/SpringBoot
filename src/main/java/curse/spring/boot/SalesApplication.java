83826230fbbe3d44551afa59d4eb961ed6e28319
Eduardo Espitalher Dias <eduardo.espitalher.93@gmail.com>
2024-07-25 15:55:00 -0300

    Migrating from JDBC to JPA.

--- src/main/java/curse/spring/boot/SalesApplication.java
+++ src/main/java/curse/spring/boot/SalesApplication.java
@@ -18,11 +18,36 @@ public class SalesApplication {
     @Bean
     public CommandLineRunner init(@Autowired Clientes clientes){
         return args -> {
-            System.out.println("Salvando clientes");
+            System.out.println("Salvando clientes...");
             clientes.salvar(new Cliente("Eduardo"));
             clientes.salvar(new Cliente("Kallel"));
 
             List<Cliente> todosClientes = clientes.obterTodos();
+            todosClientes.forEach(System.out::println);
+
+            System.out.println("Atualizando clientes...");
+            todosClientes.forEach(c -> {
+                c.setNome(c.getNome() + " atualizado.");
+                clientes.atualizar(c);
+            });
+
+            todosClientes = clientes.obterTodos();
+            todosClientes.forEach(System.out::println);
+
+            System.out.println("Buscando clientes");
+            clientes.buscarPorNome("Kal").forEach(System.out::println);
+
+            System.out.println("Deletendo clientes...");
+            clientes.obterTodos().forEach(c -> {
+                clientes.deletar(c);
+            });
+
+            todosClientes = clientes.obterTodos();
+            if (todosClientes.isEmpty()){
+                System.out.println("Nenhum cliente encontrado");
+            }else{
+                todosClientes.forEach(System.out::println);
+            };
             todosClientes.forEach(System.out::println);
        };
     }
@@ -33,3 +58,4 @@ public class SalesApplication {
     }
 
 }
+
