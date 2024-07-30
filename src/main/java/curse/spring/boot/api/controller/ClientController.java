685f046de7220014da9db7168c30773c985f4e28
Eduardo Espitalher Dias <eduardo.espitalher.93@gmail.com>
2024-07-30 15:35:45 -0300

    Mapping Requests with Request Mapping

--- src/main/java/curse/spring/boot/api/controller/ClientController.java
+++ src/main/java/curse/spring/boot/api/controller/ClientController.java
@@ -1,19 +1,22 @@
 package curse.spring.boot.api.controller;
 
 
+import curse.spring.boot.domain.entity.Cliente;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
-@RequestMapping("/api/clientes")
 public class ClientController {
 
-    @RequestMapping(value = "/hello/{nome}", method = RequestMethod.GET)
+    @RequestMapping(
+            value = {"/api/clientes/hello/{nome}",""},
+            method = RequestMethod.POST,
+            consumes = {"application/json","application/xml"},
+            produces = {"application/json", "application/xml"}
+    )
     @ResponseBody
-    public String helloCliente(@PathVariable("nome") String nomeCliente){
+    public String helloCliente(@PathVariable("nome")
+                                   String nomeCliente, @RequestBody Cliente cliente){
         return String.format("Hello %s", nomeCliente);
     }
 
