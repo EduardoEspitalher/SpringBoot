78316d612712d166703f3934f94cccd2c4fb322e
Eduardo Espitalher Dias <eduardo.espitalher.93@gmail.com>
2024-08-08 17:49:52 -0300

    using Bean Validation in the customer name field

--- src/main/java/curse/spring/boot/rest/controller/AplicationControllerAdvice.java
+++ src/main/java/curse/spring/boot/rest/controller/AplicationControllerAdvice.java
@@ -4,12 +4,16 @@ import curse.spring.boot.exception.PedidoNaoEncontradoException;
 import curse.spring.boot.exception.RegraNegocioException;
 import curse.spring.boot.rest.ApiErrors;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @RestControllerAdvice
-public class AplicationControllerAdvice {
+public class   AplicationControllerAdvice {
 
     @ExceptionHandler(RegraNegocioException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
@@ -24,4 +28,14 @@ public class AplicationControllerAdvice {
         return new ApiErrors(ex.getMessage());
     }
 
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ApiErrors handleMethodNotValidException(MethodArgumentNotValidException ex){
+        List<String> errors = ex.getBindingResult()
+                .getAllErrors()
+                .stream()
+                .map(erro -> erro.getDefaultMessage())
+                .collect(Collectors.toList());
+        return new ApiErrors(errors);
+    }
 }
